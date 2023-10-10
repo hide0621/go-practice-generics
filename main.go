f2e3b81aa8dc32d8fa3b2694293cd76707655643
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-10-10 11:32:57 +0900

    構造体をジェネリクスにした

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Vector[T any] []T
+type Pair[T any] struct {
+	First  T
+	Second T
+}
 
-type IntVector = Vector[int]
+func (p Pair[T]) PrintField() {
+	fmt.Println(p.First)
+	fmt.Println(p.Second)
+}
 
 func main() {
 
-	var v Vector[int] = []int{1, 2, 3}
-	fmt.Println(v)
+	intResult := Pair[int]{10, 20}
+	intResult.PrintField()
 
-	var v2 Vector[string] = []string{"a", "b", "c"}
-	fmt.Println(v2)
-
-	v3 := IntVector{10, 20, 30}
-	fmt.Println(v3)
-
+	intString := Pair[string]{"Hello", "World"}
+	intString.PrintField()
 }
