5f99b16f6663b5ed28d51a27f5f623eb2491cff4
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-10-12 10:10:46 +0900

    Add関数とRemove関数を追加

--- main.go
+++ main.go
@@ -8,9 +8,17 @@ func NewSet[T comparable](xs ...T) Set[T] {
 
 	s := make(Set[T])
 	for _, v := range xs {
-		s[v] = struct{}{}
+		s.Add(v)
 	}
 	return s
+}
+
+func (s Set[T]) Add(x T) {
+	s[x] = struct{}{}
+}
+
+func (s Set[T]) Remove(x T) {
+	delete(s, x)
 }
 
 func main() {
@@ -18,4 +26,6 @@ func main() {
 	a := NewSet(1, 2, 3)
 	fmt.Println(a)
 
+	a.Remove(1)
+	fmt.Println(a)
 }
